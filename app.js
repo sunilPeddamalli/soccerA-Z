b1e993865237621fd0640170eee2f467d5df927a
Sunil Peddamalli <sunilpeddamalli@gmail.com>
2021-10-19 16:18:53 -0700

    add background image for each views using css file

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
 app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, '/public')))
 
 app.get('/',(req,res)=>{
     res.send('Welcome!!!');
