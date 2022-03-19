3c27f854a83ffc9b1bf1a85e310edba86b22ef78
Sunil Peddamalli <sunilpeddamalli@gmail.com>
2022-03-18 20:00:21 -0700

    require and use passport in app.js entry file

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const matches = require('./routes/matches.js')
 const feedbacks = require('./routes/feedbacks.js')
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const passportLocal = require('passport-local');
+const User = require('./models/user');
 
 // moved to seperate route folder
 // const Match = require('./models/matches');
@@ -45,6 +48,13 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash())
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new passportLocal(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.use((req,res,next)=>{
     res.locals.success = req.flash('success');
