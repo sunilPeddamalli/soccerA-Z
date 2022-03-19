7f2ff57e040b259192db537da10444920f36fc0f
Sunil Peddamalli <sunilpeddamalli@gmail.com>
2022-03-18 21:07:19 -0700

    register user in database using post route and passport registet register method added to UserModel

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,26 @@
+const flash = require('connect-flash/lib/flash');
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const User = require('../models/user');
+const catchError = require('../utils/catchError');
 
 router.get('/register', (req,res)=>{
    res.render('user/register')
-})
+});
 
-router.post('/register', (req,res)=> {
-   res.send(req.body);
-})
+router.post('/register', catchError(async (req,res,next)=> {
+   try{
+      const {username, email, password} = req.body;
+      const user = new User ({username, email});
+      const registeredUser = await User.register(user,password);
+      flash('success', 'Welcome')
+      res.redirect('/matches')
+   }
+   catch(e){
+      req.flash('error', 'A user with given username or email-id is already registered');
+      res.redirect('/register')
+   }
+}));
 
 module.exports = router;
